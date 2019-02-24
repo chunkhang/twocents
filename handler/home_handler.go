cd675e0df4d773f1afeee9adb0018276abe7a2e3
Marcus Mu <chunkhang@gmail.com>
2019-02-25 01:49:03 +0800

    Remove comment

--- handler/home_handler.go
+++ handler/home_handler.go
@@ -7,8 +7,6 @@ import (
 )
 
 func HomeHandler(c echo.Context) error {
-	// Please note the the second parameter "home.html" is the template name and should
-	// be equal to the value stated in the {{ define }} statement in "view/home.html"
 	return c.Render(http.StatusOK, "home.html", map[string]interface{}{
 		"msg": "Hello World!",
 	})
