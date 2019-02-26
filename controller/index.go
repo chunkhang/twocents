4900ee42094a2d84778adc69f70efdecbb8d629a
Marcus Mu <chunkhang@gmail.com>
2019-02-27 01:02:50 +0800

    Handle error in controller

--- controller/index.go
+++ controller/index.go
@@ -3,12 +3,18 @@ package controller
 import (
 	"net/http"
 
+	"github.com/chunkhang/twocents/util"
 	"github.com/labstack/echo"
 )
 
 // Home is the controller for index page
-func Home(c echo.Context) error {
-	return c.Render(http.StatusOK, "index.tmpl", map[string]interface{}{
+func Home(c echo.Context) (err error) {
+	defer util.Catch(&err)
+
+	err = c.Render(http.StatusOK, "index.tmpl", map[string]interface{}{
 		"msg": "Hello World!",
 	})
+	util.Check(err)
+
+	return
 }
--- view/index.go
+++ view/index.go
@@ -27,35 +27,35 @@ type Registry struct {
 	templates map[string]*template.Template
 }
 
-// Render renders the page
-func (t *Registry) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	tmpl, ok := t.templates[name]
-	if !ok {
-		err := errors.New("Template not found -> " + name)
-		return err
+// Render renders the page with given data
+func (t *Registry) Render(w io.Writer, name string, data interface{}, c echo.Context) (err error) {
+	template, ok := t.templates[name]
+	if ok {
+		err = template.ExecuteTemplate(w, "main", data)
+	} else {
+		err = errors.New("Template not found: " + name)
 	}
-	return tmpl.ExecuteTemplate(w, "base", data)
+	return
 }
 
-// Init renders all pages
+// Init builds all the templates
 func Init() (r *Registry, err error) {
 	defer util.Catch(&err)
 	r = &Registry{}
 
-	templates := make(map[string]*template.Template)
-
 	layoutFiles, err := getFiles(layoutDirectory, fileExtension)
 	util.Check(err)
+
 	pageFiles, err := getFiles(pageDirectory, fileExtension)
 	util.Check(err)
 
+	templates := make(map[string]*template.Template)
 	for _, file := range pageFiles {
 		filename := filepath.Base(file)
 		files := append(layoutFiles, file)
-		main := template.Must(mainTemplate.Clone())
-		templates[filename] = template.Must(main.ParseFiles(files...))
+		m := template.Must(mainTemplate.Clone())
+		templates[filename] = template.Must(m.ParseFiles(files...))
 	}
-
 	r.templates = templates
 
 	return
