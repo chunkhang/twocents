ed7986ea3838444180423e8bfae050ba35e957a5
Marcus Mu <chunkhang@gmail.com>
2019-03-01 17:05:36 +0800

    Fix form

--- controller/register.go
+++ controller/register.go
@@ -11,10 +11,7 @@ import (
 func Register(c echo.Context) (err error) {
 	defer util.Catch(&err)
 
-	err = c.Render(http.StatusOK, "register.tmpl", map[string]interface{}{
-		"title":    "Hello World",
-		"subtitle": "Welcome to Two Cents",
-	})
+	err = c.Render(http.StatusOK, "register.tmpl", nil)
 	util.Check(err)
 
 	return
