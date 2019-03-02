850932bd2e0e0e1a91566aeda9959f441ce0b259
Marcus Mu <chunkhang@gmail.com>
2019-03-03 01:45:00 +0800

    Add form validation

--- controller/session.go
+++ controller/session.go
@@ -1,22 +1,31 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
 
+	valid "github.com/asaskevich/govalidator"
 	"github.com/chunkhang/twocents/util"
 	"github.com/labstack/echo"
 )
+
+type loginForm struct {
+	Email    string `form:"email" valid:"required~Email is blank,email~Invalid email format"`
+	Password string `form:"password" valid:"required~Password is blank,length(6|15)~Password length should be 6 - 15"`
+}
 
 // CreateSession creates a new session
 func CreateSession(c echo.Context) (err error) {
 	defer util.Catch(&err)
 
-	email := c.FormValue("email")
-	password := c.FormValue("password")
+	form := new(loginForm)
+	if err = c.Bind(form); err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
 
-	fmt.Printf("email = %+v\n", email)
-	fmt.Printf("password = %+v\n", password)
+	_, err = valid.ValidateStruct(form)
+	if err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
 
 	return c.Redirect(http.StatusSeeOther, "/")
 }
--- controller/user.go
+++ controller/user.go
@@ -1,24 +1,37 @@
 package controller
 
 import (
-	"fmt"
+	"errors"
 	"net/http"
 
+	valid "github.com/asaskevich/govalidator"
 	"github.com/chunkhang/twocents/util"
 	"github.com/labstack/echo"
 )
+
+type registerForm struct {
+	Email           string `form:"email" valid:"required~Email is blank,email~Invalid email format"`
+	Password        string `form:"password" valid:"required~Password is blank,length(6|15)~Password length should be 6 - 15"`
+	PasswordConfirm string `form:"password_confirm" valid:"required~Confirm Password is blank"`
+}
 
 // CreateUser creates a new user
 func CreateUser(c echo.Context) (err error) {
 	defer util.Catch(&err)
 
-	email := c.FormValue("email")
-	password := c.FormValue("password")
-	passwordConfirm := c.FormValue("password_confirm")
+	form := new(registerForm)
+	if err = c.Bind(form); err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
 
-	fmt.Printf("email = %+v\n", email)
-	fmt.Printf("password = %+v\n", password)
-	fmt.Printf("password_confirm = %+v\n", passwordConfirm)
+	_, err = valid.ValidateStruct(form)
+	if err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+
+	if form.Password != form.PasswordConfirm {
+		return c.String(http.StatusBadRequest, errors.New("Passwords do not match").Error())
+	}
 
 	return c.Redirect(http.StatusSeeOther, "/")
 }
