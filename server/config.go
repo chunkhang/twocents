568fb38d4b3d3de79b089c774ee6446b07c1f00e
Marcus Mu <chunkhang@gmail.com>
2019-03-03 14:29:54 +0800

    Configure cookies

--- server/config.go
+++ server/config.go
@@ -13,20 +13,36 @@ const (
 	staticDirectory = "static"
 	assetDirectory  = "assets"
 	logFormat       = "${time_rfc3339}: ${method} ${uri} ${status} (${latency_human})\n"
+	cookiePath      = "/"
+	cookieHTTPOnly  = true
 )
 
 func (s *server) setConfig() {
 	s.HideBanner = true
+
 	s.Static(fmt.Sprintf("/%s", staticDirectory), assetDirectory)
+
 	s.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: logFormat,
 	}))
+
 	s.Use(middleware.Gzip())
+
 	s.Use(middleware.CORS())
+
 	s.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
-		TokenLookup: "form:csrf",
+		TokenLookup:    "form:csrf",
+		CookiePath:     cookiePath,
+		CookieHTTPOnly: cookieHTTPOnly,
 	}))
+
 	s.Use(middleware.Secure())
+
 	s.Use(middleware.Recover())
-	s.Use(session.Middleware(sessions.NewCookieStore(config.StoreKey)))
+
+	store := sessions.NewCookieStore(config.StoreKey)
+	store.Options = &sessions.Options{
+		HttpOnly: cookieHTTPOnly,
+	}
+	s.Use(session.Middleware(store))
 }
